687c28cc1c967db429e882f4f8120d82fb2c9113
Kenneth Chan <kchan049@ucr.edu>
2015-04-09 07:54:37 -0700

    username and hostname now working

--- src/exec.cpp
+++ src/exec.cpp
@@ -1,45 +1,71 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <vector>
+#include <unistd.h>
+#include <cstring>
+#include <stdio.h>
+#include <cstdlib>
 
 using namespace std;
 //this function splits string into separate words and places into a vector
-vector<string> splitter(string input)
+vector<char> splitter(char input[999])
 {
-    vector <string> words;
+    vector <char> words;
     int currcount=0;//counter of current word
     string store;//storage for words that come from inputted string
 
     //there are a few cases we need to consider
     //Cases include " ", ";", "#" 
-    for(int i=0; i<input.size(); i++)
+    for(int i=0; input[i]!='\0'; i++)
     {
-        if(input.at(i)=='#')
+        if(input[i]=='#')
         {
-            if(store != "" && store!=" ")
-            words.push_back(store);
-            store = "";
+            words.push_back(input[i]);
+            words.push_back(';');
+            store="";
             break;
+        }
+        else if(input[i] == ';')
+        {
+            words.push_back(' ');
+            words.push_back(input[i]);
+            words.push_back(' ');
+        }
+        else if(input[i] == ' ')
+        {
 
         }
-        else if(input.at(i) == ';')
-        else if(input.at(i) == ' ')
+   
     }
     return words;
 }
-int main(int argc, char** argv)
+int main()
 {
-    string input;
+    string username=getlogin();
+    char hostname[999];
+    gethostname(hostname, 999);
+    string stringinput;
+    char resultstring[999];
+    char input[999];
     while(1)
     {
-        cout << "$ :" << endl; 
-        getline(cin, input);
-        vector<string> words=splitter(input);
-        for(int i=0; i<input.size(); i++)
+        cout << username << "@" << hostname << "$ ";
+        getline(cin, stringinput);
+        strcpy(input, stringinput.c_str());
+        strcpy(resultstring, stringinput.c_str());
+        vector<char> words=splitter(input);
+        for(int i=0; input!='\0'; i++)
         {        
-            cout << words.at(i);
+            cout << words[i];
         }
+        /*if(words[i]=="exit" || words[i]=="Exit")
+        {
+                exit(1);
+        }*/
     }       
     return 0;
 }
+
+
+
 
