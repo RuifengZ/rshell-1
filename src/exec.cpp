477a34c72d81dfbd52a371d295ded861eb970314
Kenneth Chan <kchan049@ucr.edu>
2015-04-15 18:37:49 -0700

    added some important comments and started working on operators like ;

--- src/exec.cpp
+++ src/exec.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <stdio.h>
 #include <cstdlib>
+#include <sys/wait.h>
 
 using namespace std;
 //this function splits string into separate words and places into an array
@@ -14,7 +15,7 @@ char* splitter(char* input)
     int currcount=0;//counter of current word
 
     //there are a few cases we need to consider
-    //Cases include " ", ";", "#" 
+    //cases include " ", ";", "#" 
     for(int i=0; input[i]!='\0'; i++)
     {
         if(input[i]=='#')
@@ -34,42 +35,72 @@ char* splitter(char* input)
 }
 int main()
 {
+    bool semicolon=0;
+    bool doubleand=0;
+    bool doubleor=0;
     string username=getlogin();
     char hostname[999];
     gethostname(hostname, 999);
     string stringinput;
-    char resultstring[999];
-    char input[999];
-    int counter=0;
     while(1)
     {
-        cout << username << "@" << hostname << "$ ";
+        //if the user name and hostname both exists, then the program displays it before the dollar sign.
+        if(getlogin() != '\0' && gethostname(hostname,999) != -1)    
+            cout << username << "@" << hostname << "$ ";
+        else{
+                perror("your hostname or username couldn't be found!");
+                cout << "$ ";
+        }
+        //takes in a string input
         getline(cin, stringinput);
-        strcpy(input, stringinput.c_str());
-        strcpy(resultstring, stringinput.c_str());
-        char *storage=strtok(input," ");
-        if(strcmp(storage,"exit")==0)
+        //this checks for exit and Exit command 
+        if(stringinput.find("exit")==0||stringinput.find("Exit")==0)
                 exit(1);
-        for(int i=0; input!='\0'; i++)
+       //this checks for comments. Basically, everything after the hash symbol is useless
+       if(stringinput.find("#") != string::npos)
+       {
+           stringinput=stringinput.substr(0,stringinput.find("#"));
+       }
+        for(int i=0; i<stringinput.size(); i++)
         {        
-            cout << storage[i];
-            int pid =fork();
+            int status=0;
+            pid_t pid = fork();
             if(pid==-1)
             {        
-                perror("This is an error with fork()");
+                perror("this is an error with fork()");
                 exit(1);
             }
             else if(pid==0)
             {
-            
+                int good=execvp(input[0],input);
+                if(good==-1)
+                {
+                        perror("Command is bad and not good!");
+                        exit(1);
+                }
+                else{
+                        cout << "Good!" << execvp(input[0],input) << endl;
+                }
             }
-            else if(pid==1)
+            else if(pid>=1)
             {
-    
+                if(waitpid(-1,&status,0)==-1)
+                {
+                        perror("Please take care of your child!");
+                        exit(1);
+                }
+                if(input[i]=="&&")
+                {
+                        if(status>0)
+                                break;
+                }
+                if(input[i]=="||")
+                        if(status<=0)
+                                break;
+
             }
-            counter++;
         }
-        /*if(words[i]=="exit" || words[i]=="Exit")
+        /*if(words[i]=="exit" || words[i]=="exit")
         {
                 exit(1);
         }*/
