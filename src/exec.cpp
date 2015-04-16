c4a0eb1ec89f248133f98e0c4d8067837dfab720
Kenneth Chan <kchan049@ucr.edu>
2015-04-15 20:32:03 -0700

    added tokenizing / parsing

--- src/exec.cpp
+++ src/exec.cpp
@@ -6,33 +6,9 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <sys/wait.h>
-
+#include<boost/tokenizer.hpp>
 using namespace std;
-//this function splits string into separate words and places into an array
-char* splitter(char* input)
-{
-    char * result;//resulting storage array
-    int currcount=0;//counter of current word
-
-    //there are a few cases we need to consider
-    //cases include " ", ";", "#" 
-    for(int i=0; input[i]!='\0'; i++)
-    {
-        if(input[i]=='#')
-        {
-            break;
-        }
-        else if(input[i] == ';')
-        {
-        }
-        else if(input[i] == ' ')
-        {
-
-        }
-   
-    }
-    return result;
-}
+using namespace boost;
 int main()
 {
     bool semicolon=0;
@@ -42,6 +18,9 @@ int main()
     char hostname[999];
     gethostname(hostname, 999);
     string stringinput;
+    int inputcnt=0;
+    char* input[999];
+    const char ands[]="&&";
     while(1)
     {
         //if the user name and hostname both exists, then the program displays it before the dollar sign.
@@ -61,49 +40,49 @@ int main()
        {
            stringinput=stringinput.substr(0,stringinput.find("#"));
        }
-        for(int i=0; i<stringinput.size(); i++)
-        {        
-            int status=0;
-            pid_t pid = fork();
-            if(pid==-1)
-            {        
-                perror("this is an error with fork()");
-                exit(1);
-            }
-            else if(pid==0)
-            {
-                int good=execvp(input[0],input);
-                if(good==-1)
-                {
-                        perror("Command is bad and not good!");
+       if(stringinput.find("&&")!=string::npos)
+                char_separator<char> sep("&&");
+                tokenizer<char_separator<char> > toker(stringinput,sep);
+                for(tokenizer<char_separator<char> >::iterator it=toker.                          begin(); it != toker.end(); it++)
+                {        
+                    int status=0;
+                    pid_t pid = fork();
+                    if(pid==-1)
+                    {        
+                        perror("this is an error with fork()");
                         exit(1);
+                    }
+                    else if(pid==0)
+                    {
+                        char_separator<char> sep(" ");
+                        tokenizer<char_separator<char> > tok(*it, sep);
+                        int counter=0;
+                        for(tokenizer<char_separator<char> >::iterator it1=tok.                          begin(); it1 != tok.end(); it1++)
+                        {
+                            counter++;
+                            input[counter]=new char[(*it1).size()];
+                            strcpy(input[counter],(*it1).c_str());
+                        }
+                        input[counter]=0;
+                        int good=execvp(input[0],input);
+                        if(good==-1)
+                        {
+                                perror("Command is bad and not good!");
+                                exit(1);
+                        }
+                        else{
+                                cout << "Good!" << execvp(input[0],input) << endl;
+                        }
+                    }
+                    else if(pid>=1)
+                    {
+                        if(waitpid(-1,&status,0)==-1)
+                        {
+                                perror("Please take care of your child!");
+                                exit(1);
+                        }
+                    }
                 }
-                else{
-                        cout << "Good!" << execvp(input[0],input) << endl;
-                }
-            }
-            else if(pid>=1)
-            {
-                if(waitpid(-1,&status,0)==-1)
-                {
-                        perror("Please take care of your child!");
-                        exit(1);
-                }
-                if(input[i]=="&&")
-                {
-                        if(status>0)
-                                break;
-                }
-                if(input[i]=="||")
-                        if(status<=0)
-                                break;
-
-            }
-        }
-        /*if(words[i]=="exit" || words[i]=="exit")
-        {
-                exit(1);
-        }*/
     }       
     return 0;
 }
