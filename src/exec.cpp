3c569d6ba7e18b67274ee25ccd197a29e9984658
Kenneth Chan <kchan049@ucr.edu>
2015-04-17 19:13:59 -0700

    exit is fully working

--- src/exec.cpp
+++ src/exec.cpp
@@ -24,172 +24,162 @@ int main()
         //if the user name and hostname both exists, then the program displays it before the dollar sign.
         if(getlogin() != '\0' && gethostname(hostname,999) != -1)    
             cout << username << "@" << hostname << "$ ";
-        else{
-                perror("your hostname or username couldn't be found!");
-                cout << "$ ";
+        else
+        {
+            perror("your hostname or username couldn't be found!");
+            cout << "$ ";
         }
         getline(cin,stringinput);
-        //this checks for exit and Exit command 
-        if(stringinput.find("exit")==0||stringinput.find("Exit")==0)
-                exit(1);
-       //this checks for comments. Basically, everything after the hash symbol is useless
-       if(stringinput.find("#") != string::npos)
-       {
-           stringinput=stringinput.substr(0,stringinput.find("#"));
-       }
-       if(stringinput.find("||")!=string::npos )
-       {
-                tokenizer<char_separator<char> > toke(stringinput,ors);
-                tokenizer<char_separator<char> >::iterator it=toke.begin();
-                for(; it != toke.end(); it++)
-                {      
-                    char_separator<char> space(" ");
-                    tokenizer<char_separator<char> > tok(*it, space);
-                    int status=0;
-                    pid_t pid = fork();
-                    if(pid==-1)
-                    {        
-                        perror("this is an error with fork()");
-                        exit(1);
-                    }
-                    else if(pid==0)
-                    {
-                        int counter=0;
-                        for(tokenizer<char_separator<char> >::iterator 
-                                it1=tok.begin(); it1 != tok.end(); 
-                                it1++,counter++)
-                        {
-                            input[counter]=new char[(*it1).size()];
-                            strcpy(input[counter],(*it1).c_str());
-                        }
-                        input[counter]=0;
-                        int good=execvp(input[0],input);
-                        if(strcmp(input[0],"exit")==0)
-                                exit(1);
-                        if(good==-1)
-                        {
-                                perror("Command is bad and not good!");
-                                exit(1);
-                        }
-                        else{
-                                cout << "Good!" << execvp(input[0],input) << endl;
-                        }
-                    }
-                    else if(pid>=1)
-                    {
-                        waitpid(-1,&status,0);
-                        if(status<=0)
-                                break;
-                        /*{
-                                perror("Please take care of your child!");
-                                exit(1);
-                        }*/
-                    }
+        //this checks for comments. Basically, everything after the hash symbol is useless
+        if(stringinput.find("#") != string::npos)
+        {
+            stringinput=stringinput.substr(0,stringinput.find("#"));
+        }
+        if(stringinput.find("||")!=string::npos )
+        {
+            tokenizer<char_separator<char> > toke(stringinput,ors);
+            tokenizer<char_separator<char> >::iterator it=toke.begin();
+            for(; it != toke.end(); it++)
+            {      
+                char_separator<char> space(" ");
+                tokenizer<char_separator<char> > tok(*it, space);
+                int status=0;
+                pid_t pid = fork();
+                if(pid==-1)
+                {        
+                    perror("this is an error with fork()");
+                    exit(1);
                 }
-            }
-           else  if(stringinput.find("&&")!=string::npos )
-            {
-                tokenizer<char_separator<char> > toke(stringinput,ands);
-                tokenizer<char_separator<char> >::iterator it=toke.begin();
-                for(; it != toke.end(); it++)
-                {      
-                    char_separator<char> space(" ");
-                    tokenizer<char_separator<char> > tok(*it, space);
-                    int status=0;
-                    pid_t pid = fork();
-                    if(pid==-1)
-                    {        
-                        perror("this is an error with fork()");
-                        exit(1);
-                    }
-                    else if(pid==0)
-                    {
-                        int counter=0;
-                        for(tokenizer<char_separator<char> >::iterator 
-                                it1=tok.begin(); it1 != tok.end(); 
-                                it1++,counter++)
-                        {
-                            input[counter]=new char[(*it1).size()];
-                            strcpy(input[counter],(*it1).c_str());
-                        }
-                        input[counter]=0;
-                        int good=execvp(input[0],input);
-                        if(strcmp(input[0],"exit")==0)
-                                exit(1);
-                       if(good==-1)
-                        {
-                                perror("Command is bad and not good!");
-                                exit(1);
-                        }
-                        else{
-                                cout << "Good!" << execvp(input[0],input) << endl;
-                        }
-                    }
-                    else if(pid>=1)
-                    {
-                        waitpid(-1,&status,0);
-                        if(status > 0)
-                            break;
-                        /*{
-                                perror("Please take care of your child!");
-                                exit(1);
-                        }*/
-                    }
-                }
-            }
-            else
+                else if(pid==0)
                 {
-                tokenizer<char_separator<char> > toke(stringinput,semico);
-                tokenizer<char_separator<char> >::iterator it=toke.begin();
-                for(; it != toke.end(); it++)
-                {      
-                    char_separator<char> space(" ");
-                    tokenizer<char_separator<char> > tok(*it, space);
-                    int status=0;
-                    pid_t pid = fork();
-                    if(pid==-1)
-                    {        
-                        perror("this is an error with fork()");
+                    int counter=0;
+                    for(tokenizer<char_separator<char> >::iterator 
+                        it1=tok.begin(); it1 != tok.end(); 
+                        it1++,counter++)
+                    {
+                        input[counter]=new char[(*it1).size()];
+                        strcpy(input[counter],(*it1).c_str());
+                    }
+                    input[counter]=0;
+                    int good=execvp(input[0],input);
+                    if(good==-1)
+                    {
+                        perror("Command is bad and not good!");
                         exit(1);
                     }
-                    else if(pid==0)
+                    else
                     {
-                        int counter=0;
-                        for(tokenizer<char_separator<char> >::iterator 
-                                it1=tok.begin(); it1 != tok.end(); 
-                                it1++,counter++)
-                        {
-                            input[counter]=new char[(*it1).size()];
-                            strcpy(input[counter],(*it1).c_str());
-                        }
-                        input[counter]=0;
-                        int good=execvp(input[0],input);
-                       if(strcmp(input[0],"exit")==0)
-                               exit(1);
-                       if(good==-1)
-                        {
-                                perror("Command is bad and not good!");
-                                exit(1);
-                        }
-                        else{
-                                cout << "Good!" << execvp(input[0],input) << endl;
-                        }
-                    }
-                    else if(pid>=1)
-                    {
-                        waitpid(-1,&status,0);
-                        /*{
-                                perror("Please take care of your child!");
-                                exit(1);
-                        }*/
+                        cout << "Good!" << execvp(input[0],input) << endl;
                     }
                 }
+                else if(pid>=1)
+                {
+                    waitpid(-1,&status,0);
+                    if(status<=0)
+                        break;
+                }
+            }
+            if(stringinput.find("exit")!=string::npos)
+                continue;
+        }
+        else if(stringinput.find("&&")!=string::npos )
+        {
+            if(stringinput.find("exit")!=string::npos)
+                exit(1);
+            tokenizer<char_separator<char> > toke(stringinput,ands);
+            tokenizer<char_separator<char> >::iterator it=toke.begin();
+            for(; it != toke.end(); it++)
+            {      
+                char_separator<char> space(" ");
+                tokenizer<char_separator<char> > tok(*it, space);
+                int status=0;
+                pid_t pid = fork();
+                if(pid==-1)
+                {        
+                    perror("this is an error with fork()");
+                    exit(1);
+                }
+                else if(pid==0)
+                {
+                    int counter=0;
+                    for(tokenizer<char_separator<char> >::iterator 
+                        it1=tok.begin(); it1 != tok.end(); 
+                        it1++,counter++)
+                    {
+                        input[counter]=new char[(*it1).size()];
+                        strcpy(input[counter],(*it1).c_str());
+                    }
+                    input[counter]=0;
+                    int good=execvp(input[0],input);
+                    if(good==-1)
+                    {
+                        perror("Command is bad and not good!");
+                        exit(1);
+                    }
+                    else
+                    {
+                        cout << "Good!" << execvp(input[0],input) << endl;
+                    }
+                }
+                else if(pid>=1)
+                {
+                    waitpid(-1,&status,0);
+                    if(status>0)
+                        break;
+                }
+            }
+        }
+        else
+        {
+            tokenizer<char_separator<char> > toke(stringinput,semico);
+            tokenizer<char_separator<char> >::iterator it=toke.begin();
+            for(; it != toke.end(); it++)
+            {      
+                char_separator<char> space(" ");
+                tokenizer<char_separator<char> > tok(*it, space);
+                int status=0;
+                pid_t pid = fork();
+                if(pid==-1)
+                {        
+                    perror("this is an error with fork()");
+                    exit(1);
+                }
+                else if(pid==0)
+                {
+                    int counter=0;
+                    tokenizer<char_separator<char> >::iterator 
+                    it1=tok.begin();
+                    for(; it1 != tok.end(); 
+                        it1++,counter++)
+                    {
+                        input[counter]=new char[(*it1).size()];
+                        strcpy(input[counter],(*it1).c_str());
+                    }
+                    input[counter]=0;
+                    
+                    int good=execvp(input[0],input);
+                    if(good==-1)
+                    {
+                        perror("Command is bad and not good!");
+                        exit(1);
+                    }
+                    else
+                    {
+                        cout << "Good!" << execvp(input[0],input) << endl;
+                    }
+                }
+                else if(pid>=1)
+                {
+                    waitpid(-1,&status,0);
+                }
+            }
+        }
+        if(stringinput.find("exit")!=string::npos)
+        {
+            exit(1);
+        }
     }
-
- 
-    }
-    return 0;
-
+    return 0; 
 }
 
 
