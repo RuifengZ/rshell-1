866cc9711f76b905ec26bbed71fa89e69ca1a519
Kenneth Chan <kchan049@ucr.edu>
2015-04-30 16:17:33 -0700

    add ls maybe ;^)

--- src/ls.cpp
+++ src/ls.cpp
@@ -13,57 +13,129 @@
 #include <pwd.h>
 #include <grp.h>
 #include <iomanip>
+#include <errno.h>
+#include <dirent.h>
+#include <vector>
+
 
 using namespace std;
 
 #define GREEN   "\033[32m" //Green color for executables
 #define BLUE    "\033[34m" //Blue color for directories
-#define GRAY  "\033[47m" //Gray color for hidden files
 #define REG   "\033[0m"//regular color
 #define HIDDEN  "\033[47m" //hidden files
+
+struct stat stats;
+DIR *directory;
+struct dirent *dir;
 
 void color(string files, struct stat name)
 {
 	//Checking Hidden Directories
-	if(files[0]=='.' &&(name.st_mode&S_IFDIR))
+	if(files.find(".") == 0  &&(name.st_mode&S_IFDIR))
 	{
 		cout << HIDDEN << BLUE;
-		cout << files;
+		cout << files << "  ";
 		cout << REG;
 	}
 	//Checking Hidden Executables
-	if(files[0]=='.' && (name.st_mode&S_IXUSR))
+	else if(files.find(".") == 0 && (name.st_mode&S_IXUSR))
 	{
 		cout << HIDDEN << GREEN;
-		cout << files;
+		cout << files << "  ";
 		cout << REG;
 	}
 	//Checking Hidden files only
-	if(files[0]=='.')
+	else if(files.find(".") == 0)
 	{
 		cout << HIDDEN;
-		cout << files;
+		cout << files << "  ";
 		cout << REG;
 	}
 	//Checking Directories
-	if(name.st_mode & S_IFDIR)
+	else if((name.st_mode & S_IFMT) == S_IFDIR)
 	{
 		cout << BLUE;
-		cout << files;
+		cout << files << "  ";
 		cout << REG;
 	}
 	//Checking executables
+	else if(name.st_mode & S_IXUSR)
 	{
-		if(name.st_mode & S_IXUSR)
-		{
-			cout << GREEN;
-			cout << files;
-			cout << REG;
-		}	
-	}
+		cout << GREEN;
+		cout << files << "  ";
+		cout << REG;
+	}	
 	//Checking regular files
-	cout << files << REG;
+	else cout << files << REG << "  " ;
 }
+
+
+void printvector(vector<string> v)
+{
+	for (unsigned int i = 0; i < v.size(); i++)
+	{
+		if (-1 == stat(v.at(i).c_str(), &stats))
+			perror("stat");
+		color(v.at(i), stats);
+	}
+}
+
+void checkdir(string s)
+{
+	if (-1 == stat(s.c_str(), &stats))
+	{	
+		perror("fuck tyou");
+		exit(1);
+	}
+	else
+	{
+		if ((stats.st_mode & S_IFMT) == S_IFREG)
+		{
+			cout<< s << endl;
+			return;
+		}
+	}
+}
+
+void printls(string s)
+{
+	checkdir(s);
+	vector<string> fag;
+	if (NULL == (directory = opendir(s.c_str())))
+		perror("hi :)");
+	errno = 0;
+	while (NULL != (dir = readdir(directory)))
+	{
+		string caa = dir->d_name;
+		if (caa.find(".") != 0)
+			fag.push_back(caa);
+
+	}
+    sort(fag.begin(), fag.end());
+	printvector(fag);
+	cout << endl;
+
+}
+
+void printals(string s)
+{
+	checkdir(s);
+	vector<string> fag;
+	if (NULL == (directory = opendir(s.c_str())))
+		perror("hi :)");
+	errno = 0;
+	while (NULL != (dir = readdir(directory)))
+	{
+		string caa = dir->d_name;
+		fag.push_back(caa);
+
+	}
+    sort(fag.begin(), fag.end());
+	printvector(fag);
+	cout << endl;
+}
+
 
 void total(vector <string> &file, string path)
 {
@@ -165,14 +237,11 @@ int main(int argc, char* argv[])
     bool l=false;//to see if ls contains -l or not
     bool R=false;//to see if ls contains -R or not
     vector <string> in;
-    vector <string> dir;
     //checking for flags
     for(int i=1; i<argc; i++)
-    {
+	{
         if(checkflag)
-        {
             if(argv[i][0]=='-')
-            {    
                 for(int j=1; argv[i][j]!=0; j++)
                 {
                     if(argv[i][j]=='R')
@@ -187,12 +256,8 @@ int main(int argc, char* argv[])
                         exit(1);
                     }
                 }
-            }
-        }
         else
-        {
             in.push_back(argv[i]);
-        }
     }
     //checking for files now
     for(int i=0; i<in.size(); i++)
@@ -205,8 +270,30 @@ int main(int argc, char* argv[])
         }
         if(!(file.st_mode & S_IFDIR))
         {
-        
+        	color(in[0],file);
+			cout << endl;
         }
     }
+	if (!a && !R && !l)
+	{
+		for (int i = 0; i < in.size(); i++)
+		{
+			cout << in.at(i);
+			printls(in.at(i));
+		}
+		if (in.size() == 0)
+			printls(".");
+	}
+	if (a and !R and !l)
+	{
+		for (int i = 0; i < in.size(); i++)
+		{
+			cout << in.at(i);
+			printals(in.at(i));
+		}
+		if (in.size() == 0)
+			printals(".");
+	}
+
     return 0;
 }
